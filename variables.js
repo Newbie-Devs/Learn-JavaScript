903f0cb1e872a86a403988e2f59d8f1c5a03b993
Federico <45139706+fededev01@users.noreply.github.com>
2020-03-22 23:50:44 +0100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,36 @@
+//Variables are containers for storing data values.
+//The value of a variable can change throughout the program.
+//Use the var keyword to declare a variable:
+var x = 10; //the value 10 is assigned to the variable x.
+
+//JavaScript is case sensitive: the variables lastName and lastname, are two different variables.
+
+//In JavaScript, the equal sign (=) is called the "assignment" operator, rather than an "equal to" operator.
+//For example, x = y will assign the value of y to x
+//A variable can be declared without a value.
+//A variable declared without a value will have the value undefined
+
+var x = 100;
+document.write(x);
+//the value 100 is assigned to the variable x and then it is writedd into the HTML document
+
+/*
+Using variables is useful in many ways.
+You might have a thousand lines of code that may include the variable x.
+When you change the value of x one time, it will automatically be changed in all places where you used it.
+*/
+
+
+//Every written "instruction" is called a statement.
+//JavaScript statements are separated by semicolons.
+
+/*
+Naming rules:
+The first character must be a letter, an underscore (_), or a dollar sign ($).
+Subsequent characters may be letters, digits, underscores, or dollar signs.
+Numbers are not allowed as the first character.
+Variable names cannot include a mathematical or logical operator in the name.
+JavaScript names must not contain spaces.
+*/
+
+//Hyphens are not allowed in JavaScript. It is reserved for subtractions.
